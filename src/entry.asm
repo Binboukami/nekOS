44e8ea9c9c987a49c136a0dcf23e4466ea341665
Binboukami <luiz.butkeivicz@gmail.com>
2024-11-30 13:29:50 -0300

    add comments to entry.asm

--- src/entry.asm
+++ src/entry.asm
@@ -1,3 +1,8 @@
+; file: entry.asm
+; description: Meant to be chainloaded at the last stage by the bootloader.
+;							 Calls the main kernel.
+;							 TODO: This file should be bundled with the main kernel code.
+
 BITS 32
 	section .text
 kernel_entry:
