f31762081dcc3e4180617d2a2aaa69e3ecc3ea00
Binboukami <luiz.butkeivicz@gmail.com>
2024-11-30 13:33:45 -0300

    housekeeping
    add comments to bochsrc and linting on disk.asm

--- src/disk.asm
+++ src/disk.asm
@@ -1,17 +1,17 @@
 disk_load:
 	mov ax, 0x03
-	mov ah, 0x0						; Reset
-	mov dl, [BOOTDRIVE]				; Drive Numeber (Floopy = 0)
+	mov ah, 0x0																			; Reset
+	mov dl, [BOOTDRIVE]															; Drive Numeber (Floopy = 0)
 	int 0x13
 
-	mov ch, 0x00					; Track 0
-	mov cl, 0x02					; Sector 2 (Next 512 bytes)
-	mov dh, 0x00					; Head number
+	mov ch, 0x00																		; Track 0
+	mov cl, 0x02																		; Sector 2 (Next 512 bytes)
+	mov dh, 0x00																		; Head number
 
 .int13_call:
-	mov ah, 0x02					; Read disk sector interrupt
-	mov al, 0x10					; Sectors to read
-	int 0x13						; Read sectors
+	mov ah, 0x02																		; Read disk sector interrupt
+	mov al, 0x10																		; Sectors to read
+	int 0x13																				; Read sectors
 
 	jc .disk_error
 
